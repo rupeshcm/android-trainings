5434874be4844b6b3430a0942046efde4f36a4dc
Sunita <sunita.s@coppermobile.com>
2018-10-26 18:59:13 +0530

    proper commit

--- app/src/main/java/com/coppermobile/mylocationmanager/MainActivity.java
+++ app/src/main/java/com/coppermobile/mylocationmanager/MainActivity.java
@@ -1,15 +1,18 @@
 package com.coppermobile.mylocationmanager;
 
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView image;
+    private TextView tvLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +20,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         image = findViewById(R.id.iv_fruit);
+        tvLocation = findViewById(R.id.tv_location);
 
         MyViewModel myViewModel = ViewModelProviders.of(this).get(MyViewModel.class);
         observeImages(myViewModel);
     }
 
     private void observeImages(MyViewModel myViewModel) {
-        myViewModel.getFruits().observe(this, new Observer<Integer>() {
-            @Override
-            public void onChanged(@Nullable Integer integer) {
+        myViewModel.getFruits().observe(this, integer -> {
+            if (integer != null) {
                 image.setImageResource(integer);
             }
         });
+    }
+
+    private class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            tvLocation.setText(String.format("%s, %s", location.getLatitude(), location.getLongitude()));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Toast.makeText(MainActivity.this, "Provider enabled: " + provider, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
     }
 }
